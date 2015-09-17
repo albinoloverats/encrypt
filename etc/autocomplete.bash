9b33e6eac72db0cf18cf73c6c65e3f765eb1a355
The encrypt Development Team <encrypt@albinoloverats.net>
2015-09-17 14:27:58 +0100

    Update miscellaneous files (autocomplete, mime)

--- etc/autocomplete.bash
+++ etc/autocomplete.bash
@@ -1,44 +1,44 @@
 _encrypt()
 {
-    local cur prev opts 
-    COMPREPLY=()
-    cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
+	local cur prev opts
+	COMPREPLY=()
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-    opts="-h --help -v --version -l --licence \
-          -k --key -p --password \
-          -q --quiet -d --debug"
+	opts="-h --help -v --version -l --licence \
+		  -k --key -p --password \
+		  -q --quiet -d --debug"
 
-    [ "$1" == "encrypt" ] && opts="$opts -c --cipher -s --hash -x --no-compress"
+	[ "$1" == "encrypt" ] && opts="$opts -c --cipher -s --hash -x --no-compress"
 
-    if [[ "${cur}" == -* ]]
-    then
-        COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
-    else
-        case "${prev}" in
-            -h|--help|-v|--version|-l|--licence)
-                ;;
-            -k|--key)
-                COMPREPLY=($(compgen -A file -- "${cur}"))
-                ;;
-            -d|--debug)
-                COMPREPLY=($(compgen -W "everything verbose debug info warning error fatal" -- "${cur}"))
-                ;;
-            -c|--cipher)
-                COMPREPLY=($(compgen -W "list $(encrypt -c list 2>&1 | tr '[A-Z]' '[a-z]')" -- "${cur}"))
-                ;;
-            -s|--hash)
-                COMPREPLY=($(compgen -W "list $(encrypt -s list 2>&1 | tr '[A-Z]' '[a-z]')" -- "${cur}"))
-                ;;
-            -p|--password|-x|--no-compress)
-                ;;
-            *)
-                COMPREPLY=($(compgen -A file -- "${cur}"))
-                ;;
-        esac
-    fi
+	if [[ "${cur}" == -* ]]
+	then
+		COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
+	else
+		case "${prev}" in
+			-h|--help|-v|--version|-l|--licence)
+				;;
+			-k|--key)
+				COMPREPLY=($(compgen -A file -- "${cur}"))
+				;;
+			-c|--cipher)
+				COMPREPLY=($(compgen -W "list $(encrypt -c list 2>&1 | tr '[A-Z]' '[a-z]')" -- "${cur}"))
+				;;
+			-s|--hash)
+				COMPREPLY=($(compgen -W "list $(encrypt -s list 2>&1 | tr '[A-Z]' '[a-z]')" -- "${cur}"))
+				;;
+			-m|--mode)
+				COMPREPLY=($(compgen -W "list $(encrypt -m list 2>&1 | tr '[A-Z]' '[a-z]')" -- "${cur}"))
+				;;
+			-p|--password|-x|--no-compress|-g|--no-gui|-f|--follow|-b|--back-compat|-r|--raw)
+				;;
+			*)
+				COMPREPLY=($(compgen -A file -- "${cur}"))
+				;;
+		esac
+	fi
 
-    return 0
+	return 0
 }
 
 complete -F _encrypt encrypt decrypt
--- etc/autocomplete.zsh
+++ etc/autocomplete.zsh
@@ -1,38 +1,44 @@
 #compdef encrypt decrypt
 
-local args ciphers hashes state
+local args ciphers hashes modes state
 
 args=(
-  '(-h --help)'{-h,--help}'[Display this message]'
-  '(-l --licence)'{-l,--licence}'[Display GNU GPL v3 licence header]'
-  '(-v --version)'{-v,--version}'[Display application version]'
-  '(-d --debug)'{-d,--debug=}'[Turn on debugging (to specified level)]: :->debug'
-  '(-q --quiet)'{-q,--quiet}'[Turn off all but serious error messages]'
-  '(-k --key)'{-k,--key=}'[File whose data will be used to generate the key]:Key:_files'
-  '(-p --password)'{-p,--password=}'[Password used to generate the key]:Password:'
+	'(-h --help)'{-h,--help}'[Display this message]'
+	'(-l --licence)'{-l,--licence}'[Display GNU GPL v3 licence header]'
+	'(-v --version)'{-v,--version}'[Display application version]'
+	'(-k --key)'{-k,--key=}'[File whose data will be used to generate the key]:Key:_files'
+	'(-p --password)'{-p,--password=}'[Password used to generate the key]:Password:'
+	'(-r --raw)'{-r,--raw}'[Don’t generate or look for an encrypt header; this IS NOT recommended, but can be usefull in some (limited) situations.]'
 )
 
 if [[ $service = encrypt ]]
 then
-    args+=(
-      '(-c --cipher)'{-c,--cipher=}'[Algorithm to use to encrypt data]: :->ciphers'
-      '(-s --hash)'{-s,--hash=}'[Hash algorithm to generate key]: :->hashes'
-      '(-x --no-compress)'{-x,--no-compress}'[Do not compress the plaintext using the xz algorithm]'
-    )
+	args+=(
+		'(-c --cipher)'{-c,--cipher=}'[Algorithm to use to encrypt data]: :->ciphers'
+		'(-s --hash)'{-s,--hash=}'[Hash algorithm to generate key]: :->hashes'
+		'(-m --mode)'{-s,--hash=}'[The encryption mode to use]: :->mdoes'
+		'(-x --no-compress)'{-x,--no-compress}'[Do not compress the plaintext using the xz algorithm]'
+		'(-f --follow)'{-f,--follow}'[Follow symlinks, the default is to store the link itself]'
+		'(-b --back-compat)'{-b,--back-compat}'[Create an encrypted file that is backwards compatible]'
+	)
 fi
 
 _arguments -C -s "$args[@]" '*:files:_files'
 
 case $state in
-    debug)
-        compadd everything verbose debug info warning error fatal
-        ;;
+	debug)
+		compadd everything verbose debug info warning error fatal
+		;;
 
-    ciphers)
-        compadd list ${=${(f)"$($service -c list 2>&1 | tr '[A-Z]' '[a-z]')"}:#*\:}
-        ;;
+	ciphers)
+		compadd list ${=${(f)"$($service -c list 2>&1 | tr '[A-Z]' '[a-z]')"}:#*\:}
+		;;
 
-    hashes)
-        compadd list ${=${(f)"$($service -s list 2>&1 | tr '[A-Z]' '[a-z]')"}:#*\:}
-        ;;
+	hashes)
+		compadd list ${=${(f)"$($service -s list 2>&1 | tr '[A-Z]' '[a-z]')"}:#*\:}
+		;;
+
+	modes)
+		compadd list ${=${(f)"$($service -m list 2>&1 | tr '[A-Z]' '[a-z]')"}:#*\:}
+		;;
 esac
