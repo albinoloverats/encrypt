eefbc2180e2034fc5a8484c38b633a7e1b2c6182
The encrypt Android and OS X Development Team <encrypt@albinoloverats.net>
2017-03-28 14:49:23 +0100

    Add OS X specific common code

---
+++ src/common/osx.h
@@ -0,0 +1,30 @@
+/*
+ * Common code which is used to call Objective-C methods on OS X
+ * Copyright © 2005-2017, albinoloverats ~ Software Development
+ * email: webmaster@albinoloverats.net
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef _OSX_H_
+#define _OSX_H_
+
+#ifdef __APPLE__
+
+void osx_open_file(char *path);
+
+#endif
+
+#endif
