efefea13cac1456c3eef1527217ed227d36aead9
The encrypt Development Team <encrypt@albinoloverats.net>
2021-01-21 19:50:20 +0000

    Update copyright notice year

--- src/encrypt.h
+++ src/encrypt.h
@@ -1,6 +1,6 @@
 /*
  * encrypt ~ a simple, multi-OS encryption utility
- * Copyright © 2005-2020, albinoloverats ~ Software Development
+ * Copyright © 2005-2021, albinoloverats ~ Software Development
  * email: encrypt@albinoloverats.net
  *
  * This program is free software: you can redistribute it and/or modify
@@ -24,7 +24,7 @@
 /*!
  * \file    encrypt.h
  * \author  Ashley M Anderson
- * \date    2009-2020
+ * \date    2009-2021
  * \brief   Main encryption routine
  *
  * The main encryption routine; the only visible function is for the
